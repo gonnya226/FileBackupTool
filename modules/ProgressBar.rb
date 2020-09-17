07b51868a2d1adea48feab1e300b8ec5e8b32171
gonnya <gonnya226@gmail.com>
2020-09-17 15:41:18 +0900

    最初の実装

--- modules/ProgressBar.rb
+++ modules/ProgressBar.rb
@@ -1,31 +1,87 @@
 class ProgressBar
 
-    # 基本設定 
-        ## フォーマット
-            ### バーの長さ（半角５０文字くらい、一文字 2%）
-            ### バーに用いる記号（未達、達成分、開始端、終端）
-            ### 処理中情報の表示（ファイル/フォルダー名）
+    # 表示フォーマット用の文字列 
+    BAR = ("|" + ("-"*9 + "+")*5).chop + "|"    # プログレスバーのベース文字列（固定50文字分、1文字2%）
+    DONE = "="                                  # 済み記号
+    ARROW = ">"                                 # 済み記号の先端
+    
+    ### 処理中情報の表示（ファイル/フォルダー名）
             ### 割合表示の表示形式
                 #### 達成分サイズ、未達分サイズ、割合　の３つを表示？
 
     # プログレスバーの初期化
-        ## 新規にインスタンス生成して、初期化⇒開始とする。
-        ## 進捗度の初期化（達成分数値、100%時の数値を受け取っ'てこちらで計算する方が良いか？）
-        ## その他、クラス内で使用する変数の初期化
+    #   total の値を受け取って、初期値とする。
+    def initialize(name, total)
 
+        # インスタンス変数
+        @total = total      # 100% 時の値（単位はfilesizeでもファイル個数でも何でもいい。）
+        @name = name        # 進捗表示する処理名（フォルダー名とか渡す）
+        @progress = 0       # 進捗（0% ～ 100%）
+        @done = 0           # 完了した値
+        @line = ""          # 表示する内容
+    end
+    
     # プログレスバーの表示更新
-        ## 進捗分、処理中情報の受け取り
-            ### ? 今処理してる分のサイズを受け取って、加算形式にするか？
-                #### 計算上、100% を超えててしまったら、「100%超」として表示する。
+    def show(info, filesize)
+        
+        # 処理したファイルサイズを加算し、進捗割合を計算
+        @done += filesize        
+        getProgress
 
-        ## 情報表示
-            ### 達成割合から、表示する記号を調整
-            ### キャリッジリターン `\r` して、上書きする。
+        @line = BAR.dup     # BAR の内容を複製（こうしないと BAR の値が変わってしまう。。）
 
-    # 終了処理
-        ## close メソッドで明示的に終了させる。（終了判断はクラス内では行わない。）
-        ## 改行`\n`して、次の表示に備える。
-        ## オブジェクト破棄は行わない。（呼び出し側で参照を外すなりしてもらう。） 
+        if @progress < 2 then
+            # 進捗無し。何もしない。
+        elsif @progress >= 100 then
+            @line[1..-2] = DONE*(BAR.length-2)  # 両端を除く全てを DONE 記号で埋める。
+        else
+            tmp = "|" + (DONE*(@progress/2).floor).chop + ARROW     # 進捗部分を生成
+            @line[0..tmp.length-1] = tmp                            # 進捗部分を置き換える。
+        end
+        
+        # 情報表示
+        print sprintf("\r%-10s (%s/%s) %s %sをコピー中", @name, getFilesizeString(@done), getFilesizeString(@total), @line, info )
+
+    end
+    
+    # 進捗状態をリセットする。
+    def reset
+        @progress = 0
+        @done = 0
+    end
+
+    private
+
+    # 進捗割合を取得する。
+    def getProgress
+        if @total <= 0 then
+            # total が 0 以下の値：常に100% として返す。
+            @progress = 100
+        elsif @total < @done
+            # total が done よりも小さい：101% として返す。
+            @progress = 101
+        else
+            # それ以外の場合は、計算して返す。値は四捨五入で返す。
+            @progress = ((@done*100)/@total).round
+        end
+    end
+
+    # ファイルサイズ文字列を返す。(size は、バイト数で来ることを想定)
+    def getFilesizeString(size)
+
+        unit = ["Bytes", "KiB", "MiB", "Gib"]
+        block = 1024
+
+        for n in 0..3 do
+            if size.between?(block**n, block**(n+1)) then
+                return ((size/block**n).floor).to_s + unit[n]
+            end
+        end
+
+        # どの単位にも合致しない場合（TiB以上か、負数）は、そのまま Bytes で返す。
+        return size + "Bytes"
+
+    end
 
 end
 
